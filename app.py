edcfd76456a2d2fd2176a430ac6f6b04f9fa67fa
lanlan-21 <m29pfzv8t7@privaterelay.appleid.com>
2026-09-05 05:18:11 +0800

    Update app.py

--- app.py
+++ app.py
@@ -73,5 +73,5 @@ def on_update_text(data):
 import os
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     socketio.run(app, host='0.0.0.0', port=port, allow_unsafe_werkzeug=True)
