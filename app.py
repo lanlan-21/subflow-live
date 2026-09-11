22bde9fba163d58e84af2a15b1e492f54e9e58e6
lanlan-21 <m29pfzv8t7@privaterelay.appleid.com>
2026-09-11 20:31:23 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,150 +1,134 @@
-import os
-import secrets
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, request, redirect, session, url_for
 from flask_socketio import SocketIO, emit, join_room, leave_room
+import uuid
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'subflow-simple'
-socketio = SocketIO(app, cors_allowed_origins="*", async_mode='threading')
+app.secret_key = "steno_private_secret_key_2026"
 
-DEFAULT_ROOM_CONFIG = {
-    'text': '',
-    'font': '"DFKai-SB", "BiauKai", "標楷體", serif',
-    'size': 48,
-    'line': '1.1',
-    'color': '#ffff33',
-    'bg': '#121212',
-    'scale': 100,
-    'pad_x': 8,
-    'pad_y': 10,
-    'master_sid': None
-}
+# 🌟 設定後台管理密碼（可自由修改）
+SITE_PASSWORD = "8888"
 
-rooms_data = {
-    'A': dict(DEFAULT_ROOM_CONFIG),
-    'B': dict(DEFAULT_ROOM_CONFIG),
-    'C': dict(DEFAULT_ROOM_CONFIG)
-}
+# 關閉持久化儲存，確保「關閉瀏覽器即登出」
+app.config['SESSION_PERMANENT'] = False
 
-room_editors = {}
+socketio = SocketIO(app, cors_allowed_origins="*")
 
-def get_room_id(raw_room):
-    return str(raw_room).strip() if raw_room else 'A'
+room_masters = {}   # room_id -> master_sid
+room_editors = {}   # room_id -> set of editor sids
+
+@app.before_request
+def require_login():
+    # 1. 允許登入頁面與靜態檔案
+    if request.endpoint in ['login', 'static']:
+        return None
+    
+    # 2. 🌟 觀眾區全面免密碼：網址開頭為 /view/ 者直接放行
+    if request.path.startswith('/view/'):
+        return None
+    
+    # 3. 首頁、/edit/ 工作台等管理頁面需驗證密碼
+    if not session.get('authenticated'):
+        # 記錄使用者原本想去的網址，登入後自動轉跳回來
+        session['next_url'] = request.url
+        return redirect(url_for('login'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        user_input_pass = request.form.get('password', '')
+        if user_input_pass == SITE_PASSWORD:
+            session['authenticated'] = True
+            # 登入成功後，轉跳回原本想開啟的頁面或首頁
+            next_page = session.pop('next_url', url_for('index'))
+            return redirect(next_page)
+        else:
+            error = "密碼錯誤，請重新輸入！"
+    return render_template('login.html', error=error)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/new_room')
-def new_room():
-    room_id = secrets.token_hex(4)
-    rooms_data[room_id] = dict(DEFAULT_ROOM_CONFIG)
-    return redirect(url_for('edit_room', room_id=room_id))
-
 @app.route('/edit/<room_id>')
 def edit_room(room_id):
-    r = get_room_id(room_id)
-    if r not in rooms_data:
-        rooms_data[r] = dict(DEFAULT_ROOM_CONFIG)
-    return render_template('edit.html', room_id=r)
+    return render_template('edit.html', room_id=room_id)
 
 @app.route('/view/<room_id>')
 def view_room(room_id):
-    r = get_room_id(room_id)
-    if r not in rooms_data:
-        rooms_data[r] = dict(DEFAULT_ROOM_CONFIG)
-    return render_template('view.html', room_id=r)
+    return render_template('view.html', room_id=room_id)
 
-@app.route('/display/<room_id>')
-def display_room(room_id):
-    r = get_room_id(room_id)
-    if r not in rooms_data:
-        rooms_data[r] = dict(DEFAULT_ROOM_CONFIG)
-    return render_template('display.html', room_id=r)
+@app.route('/new_room')
+def new_room():
+    unique_id = str(uuid.uuid4())[:8]
+    return redirect(f'/edit/{unique_id}')
 
 @socketio.on('join')
 def on_join(data):
-    room = get_room_id(data.get('room', 'A'))
+    room = data.get('room')
     is_editor = data.get('is_editor', False)
     sid = request.sid
 
     join_room(room)
 
-    if room not in rooms_data:
-        rooms_data[room] = dict(DEFAULT_ROOM_CONFIG)
-    if room not in room_editors:
-        room_editors[room] = []
-
     if is_editor:
-        if sid not in room_editors[room]:
-            room_editors[room].append(sid)
-        
-        # 指派主投影
-        if not rooms_data[room].get('master_sid') or rooms_data[room]['master_sid'] not in room_editors[room]:
-            rooms_data[room]['master_sid'] = sid
+        if room not in room_editors:
+            room_editors[room] = set()
+        room_editors[room].add(sid)
 
-        broadcast_roles(room)
+        if room not in room_masters or room_masters[room] is None:
+            room_masters[room] = sid
 
-    current_state = dict(rooms_data[room])
-    emit('sync_text', current_state)
+        broadcast_role_status(room)
+
+@socketio.on('claim_master')
+def on_claim_master(data):
+    room = data.get('room')
+    sid = request.sid
+    if room in room_editors and sid in room_editors[room]:
+        room_masters[room] = sid
+        broadcast_role_status(room)
+
+@socketio.on('update_text')
+def on_update_text(data):
+    room = data.get('room')
+    sid = request.sid
+    data['sender_sid'] = sid
+    data['is_master'] = (room_masters.get(room) == sid)
+    emit('sync_text', data, to=room, include_self=False)
+
+@socketio.on('cursor_move')
+def on_cursor_move(data):
+    room = data.get('room')
+    sid = request.sid
+    emit('cursor_update', {
+        'sid': sid,
+        'cursor_index': data.get('cursor_index', 0)
+    }, to=room, include_self=False)
 
 @socketio.on('disconnect')
 def on_disconnect():
     sid = request.sid
-    for room, editors in room_editors.items():
+    for room, editors in list(room_editors.items()):
         if sid in editors:
             editors.remove(sid)
-            if rooms_data[room].get('master_sid') == sid:
-                rooms_data[room]['master_sid'] = editors[0] if editors else None
-            broadcast_roles(room)
-            # 廣播某位聽打員離線，清除其游標
             emit('cursor_remove', {'sid': sid}, to=room)
-            break
 
-@socketio.on('claim_master')
-def on_claim_master(data):
-    room = get_room_id(data.get('room'))
-    sid = request.sid
-    if room in rooms_data:
-        rooms_data[room]['master_sid'] = sid
-        broadcast_roles(room)
+            if room_masters.get(room) == sid:
+                if len(editors) > 0:
+                    room_masters[room] = next(iter(editors))
+                else:
+                    room_masters[room] = None
 
-def broadcast_roles(room):
-    emit('role_status_update', {
-        'master_sid': rooms_data[room].get('master_sid'),
-        'total_editors': len(room_editors.get(room, []))
+            broadcast_role_status(room)
+
+def broadcast_role_status(room):
+    total = len(room_editors.get(room, set()))
+    master = room_masters.get(room)
+    socketio.emit('role_status_update', {
+        'master_sid': master,
+        'total_editors': total
     }, to=room)
 
-@socketio.on('update_text')
-def on_update_text(data):
-    room = get_room_id(data.get('room', 'A'))
-    sid = request.sid
-    if room not in rooms_data:
-        rooms_data[room] = dict(DEFAULT_ROOM_CONFIG)
-
-    if 'text' in data:
-        rooms_data[room]['text'] = data['text']
-
-    cur_master = rooms_data[room].get('master_sid')
-    is_master = (cur_master == sid) or (cur_master is None)
-
-    if is_master:
-        for k in ['font', 'size', 'line', 'color', 'bg', 'scale', 'pad_x', 'pad_y']:
-            if k in data:
-                rooms_data[room][k] = data[k]
-
-    data['is_master'] = is_master
-    data['sender_sid'] = sid  # 明確標註發送者 Socket ID
-    emit('sync_text', data, to=room, include_self=False)
-
-# 游標廣播：帶上真正的原生 sid
-@socketio.on('cursor_move')
-def on_cursor_move(data):
-    room = get_room_id(data.get('room'))
-    if room:
-        data['sid'] = request.sid
-        emit('cursor_update', data, to=room, include_self=False)
-
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 10000))
-    socketio.run(app, host='0.0.0.0', port=port, allow_unsafe_werkzeug=True)
+    socketio.run(app, host='0.0.0.0', port=10000, debug=True)
